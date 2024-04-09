5750c6bad48f393f7a1722759f72c793d6a098c8
Jonathan Hoyt <jonmagic@gmail.com>
2024-04-09 13:18:01 -0700

    Update test names to be more readable

--- test/test_to_regexp.rb
+++ test/test_to_regexp.rb
@@ -2,43 +2,43 @@
 require 'helper'
 
 describe "String#to_regexp" do
-  it "test_000_versus_eval_ascii" do
+  it "compares ASCII string to regular expression" do
     str = "/finalis(e)/im"
     old_way = eval(str)
     new_way = str.to_regexp
     assert_equal old_way, new_way
   end
 
-  it "test_000a_versus_eval_utf8" do
+  it "compares UTF-8 string to regular expression" do
     str = "/finalis(é)/im"
     old_way = eval(str)
     new_way = str.to_regexp
     assert_equal old_way, new_way
   end
 
-  it "test_001_utf8" do
+  it "matches UTF-8 character in a string" do
     assert_equal 'ë', '/(ë)/'.to_regexp.match('Citroën').captures[0]
   end
 
-  it "test_002_multiline" do
+  it "matches multiline string to regular expression" do
     assert_nil '/foo.*(bar)/'.to_regexp.match("foo\n\nbar")
     assert_equal 'bar', '/foo.*(bar)/m'.to_regexp.match("foo\n\nbar").captures[0]
   end
 
-  it "test_003_ignore_case" do
+  it "matches string to regular expression with ignore case" do
     assert_nil '/(FOO)/'.to_regexp.match('foo')
     assert_equal 'foo', '/(FOO)/i'.to_regexp.match('foo').captures[0]
   end
 
-  it "test_004_percentage_r_notation" do
+  it "matches string to regular expression with percentage r notation" do
     assert_equal '/', '%r{(/)}'.to_regexp.match('/').captures[0]
   end
 
-  it "test_005_multiline_and_ignore_case" do
+  it "matches multiline string to regular expression with ignore case" do
     assert_equal 'bar', '/FOO.*(BAR)/mi'.to_regexp.match("foo\n\nbar").captures[0]
   end
 
-  it "test_006_cant_fix_garbled_input" do
+  it "handles garbled input" do
     if RUBY_VERSION >= '1.9'
       garbled = 'finalisé'.force_encoding('ASCII-8BIT') # like if it was misinterpreted
       assert_raises(Encoding::CompatibilityError) do
@@ -52,7 +52,7 @@ describe "String#to_regexp" do
     end
   end
 
-  it "test_007_possible_garbled_input_fix_using_manfreds_gem" do
+  it "fixes garbled input using Manfred's gem" do
     if RUBY_VERSION >= '1.9'
       require 'ensure/encoding'
       garbled = 'finalisé'.force_encoding('ASCII-8BIT') # like if it was misinterpreted
@@ -63,33 +63,33 @@ describe "String#to_regexp" do
     end
   end
 
-  it "test_008_as_regexp" do
+  it "converts string to regular expression with as_regexp method" do
     str = '/finalis(é)/in'
     assert_equal ['finalis(é)', ::Regexp::IGNORECASE, 'n'], str.as_regexp
     assert_equal Regexp.new(*str.as_regexp), str.to_regexp
   end
 
-  it "test_009_ruby_19_splat" do
+  it "returns nil when converting a non-regular expression string" do
     assert_nil 'hi'.to_regexp
   end
 
-  it "test_010_regexp_to_regexp" do
+  it "converts regular expression to regular expression" do
     a = /foo/
     assert_equal a, a.to_regexp
   end
 
-  it "test_011_ignore_case_option" do
+  it "matches string to regular expression with ignore case option" do
     assert_nil '/(FOO)/'.to_regexp(:ignore_case => false).match('foo')
     assert_nil '/(FOO)/'.to_regexp(:ignore_case => false).match('foo')
     assert_equal 'foo', '/(FOO)/'.to_regexp(:ignore_case => true).match('foo').captures[0]
     assert_equal 'foo', '/(FOO)/i'.to_regexp(:ignore_case => true).match('foo').captures[0]
   end
 
-  it "test_012_literal_option" do
+  it "matches string to regular expression with literal option" do
     assert '/(FOO)/'.to_regexp(:literal => true).match('hello/(FOO)/there')
   end
 
-  it "test_013_combine_literal_and_ignore_case" do
+  it "combines literal and ignore case options" do
     assert '/(FOO)/'.to_regexp(:literal => true, :ignore_case => true).match('hello/(foo)/there')
 
     # can't use inline options obviously
@@ -97,7 +97,7 @@ describe "String#to_regexp" do
     assert '/(FOO)/i'.to_regexp(:literal => true).match('hello/(FOO)/ithere')
   end
 
-  it "test_014_try_convert" do
+  it "tries to convert string to regular expression" do
     if RUBY_VERSION >= '1.9'
       assert_equal /foo/i, Regexp.try_convert('/foo/i')
       assert_equal //, Regexp.try_convert('//')
@@ -113,7 +113,7 @@ describe "String#to_regexp" do
     '"',
     "/"
   ]
-  it "test_015_union" do
+  it "tests union of regular expressions" do
     assert_equal /penzance/, Regexp.union('penzance')
     assert_equal /skiing|sledding/, Regexp.union('skiing', 'sledding')
     assert_equal /skiing|sledding/, Regexp.union(['skiing', 'sledding'])
@@ -123,7 +123,7 @@ describe "String#to_regexp" do
     assert_equal %r{&|<|>|'|"|\/}.inspect, Regexp.union(*ESCAPE_HTML_KEYS).inspect
   end
 
-  it "test_016_detect" do
+  it "detects regular expression in a string" do
     assert_nil ''.to_regexp(:detect => true)
     assert_equal //, '//'.to_regexp(:detect => true)
     assert_equal /foo/, 'foo'.to_regexp(:detect => true)
@@ -136,7 +136,7 @@ describe "String#to_regexp" do
   end
 
   # https://github.com/ruby/ruby/blob/trunk/test/ruby/test_regexp.rb#L474 "test_union2"
-  it "test_mri_union2" do
+  it "tests union of regular expressions in MRI" do
     assert_equal(/(?!)/, Regexp.union)
     assert_equal(/foo/, Regexp.union(/foo/))
     assert_equal(/foo/, Regexp.union([/foo/]))
@@ -146,7 +146,7 @@ describe "String#to_regexp" do
   end
 
   # https://github.com/ruby/ruby/blob/trunk/test/ruby/test_regexp.rb#L464 "test_try_convert"
-  it "test_mri_try_convert" do
+  it "tries to convert string to regular expression in MRI" do
     assert_equal(/re/, Regexp.try_convert(/re/))
     assert_nil(Regexp.try_convert("re"))
 
@@ -157,19 +157,19 @@ describe "String#to_regexp" do
   end
 
   # https://github.com/jruby/jruby/blob/master/spec/ruby/core/regexp/try_convert_spec.rb#L5
-  it "test_jruby_returns_argument_if_given_regexp" do
+  it "returns argument if given regular expression in JRuby" do
     assert_equal /foo/s, Regexp.try_convert(/foo/s)
   end
 
   # https://github.com/jruby/jruby/blob/master/spec/ruby/core/regexp/try_convert_spec.rb#L9
-  it "test_jruby_returns_nil_if_given_arg_cant_be_converted" do
+  it "returns nil if given argument can't be converted in JRuby" do
     ['', 'glark', [], Object.new, :pat].each do |arg|
       assert_nil Regexp.try_convert(arg)
     end
   end
 
   # https://github.com/jruby/jruby/blob/master/test/externals/ruby1.9/uri/test_common.rb#L32
-  it "test_jruby_uri_common_regexp" do
+  it "tests URI common regular expression in JRuby" do
     assert_instance_of Regexp, URI.regexp
     assert_instance_of Regexp, URI.regexp(['http'])
     assert_equal URI.regexp, URI.regexp
@@ -184,7 +184,7 @@ describe "String#to_regexp" do
   end
 
   # https://github.com/jruby/jruby/blob/master/spec/ruby/core/regexp/union_spec.rb#L14
-  it "test_jruby_quotes_string_arguments" do
+  it "quotes string arguments in JRuby" do
     assert_equal /n|\./, Regexp.union("n", ".")
   end
 end
